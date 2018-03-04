f947c2f4ac5d53c27bef830adfbfcaa46490e5bb
Lise Gaule <gaulela@gmail.com>
2018-03-04 22:11:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function shout(string)
-{
-  return string
+function shout(string){
+  return string.toUpperCase()
 }
-'Hello!'.toUpperCase()
-//'HELLO!'
 
-function shout(string)
-{
-  return
-  string.toUpperCase()
+function whisper(string){
+return string.toLowerCase()
+}
+
+function sayHiToGrandma(string){
+  return string.toUpperCase()
 }
